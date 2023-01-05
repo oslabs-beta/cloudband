f452d34e5a57384384e04bfb0d1cd257269b809b
Cam <102944566+CamSalter@users.noreply.github.com>
2023-01-04 19:24:38 -0500

    created landing page with chart, dummy data and basic styling

--- webpack.config.js
+++ webpack.config.js
@@ -31,12 +31,24 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.js#/,
+        test: /\.module\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.jsx?/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ['@babel/preset-env',  `@babel/preset-react`],
           },
         },
       },
