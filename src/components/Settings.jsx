85fa1e108473b8e051598edbe6e6139c6579d6c3
kimballcaroline <kimballcaroline@gmail.com>
2023-01-19 14:59:20 -0500

    Set state for Ec2Metric on dropdown change

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import '../componentStyling/Settings.scss';
 
   const { ec2Metric, setEc2Metric } = props;
+  const onChange = (event) => {
+    setEc2Metric(event.target.value);
+  };
   return (
     <div className="settings-wrapper">
       <h2>Settings</h2>
