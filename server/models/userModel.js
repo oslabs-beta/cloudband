efa86e3a2951259bbffce65959e52b90ed0bcdae
John Donovan <jodonovan845@gmail.com>
2023-01-18 20:06:26 -0500

    minor adjustment to userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -33,12 +33,12 @@ userSchema.pre('save', function (next) {
   });
 });
 
-userSchema.methods.comparePassword = function (candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
-};
+// userSchema.methods.comparePassword = function (candidatePassword, cb) {
+//   bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+//     if (err) return cb(err);
+//     cb(null, isMatch);
+//   });
+// };
 // any time we save a document to our users collection (by calling User.save(...))
 // run this function BEFORE saving the document
 // userSchema.pre('save', async function (next) {
