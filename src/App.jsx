406c34513ef6e110b90c470ec9651684698b683a
kimballcaroline <kimballcaroline@gmail.com>
2023-01-21 14:58:19 -0500

    Built switch for main App render

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
 import MainContainer from './containers/MainContainer.jsx';
+import Login from './components/Login.jsx';
+import Signup from './components/Signup.jsx';
+import LandingPage from './components/LandingPage.jsx';
 import './styles.scss';
 
 const App = () => {
+  const [showPage, setShowPage] = useState('landing-page');
+
+  const switchPage = () => {
+    if (showPage === 'landing-page') {
+      return <LandingPage />;
+    } else if (showPage === 'signup') {
+      return <Signup />;
+    } else if (showPage === 'login') {
+      console.log('showPage in App: ', showPage);
+      return <MainContainer />;
+    }
+  };
+
+  useEffect(() => {
+    switchPage();
+  }, [showPage]);
+
   return (
     <div className="app">
-      <Navbar />
+      <Navbar setShowPage={setShowPage} />
       <hr />
-      <MainContainer />
+      {switchPage}
       <Footer />
     </div>
   );
