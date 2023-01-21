2fe3037bcfdb49608e51d9e7e7f336ffd72085f9
John Donovan <jodonovan845@gmail.com>
2023-01-21 00:09:04 -0500

    fixed slash in the import App line

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from "react";
-import App from ".//App.jsx";
+import React from 'react';
+import App from './App.jsx';
 //import { render } from "react-dom"; --> deprecated, apparently. createRoot method (see below) is part of the new API for rendering React components in the DOM, which was introduced in React 18. This API replaces the old render method, which has been deprecated and will be removed in a future version of React.
-
 
 // import "./stylesheets/styles.scss";
 import { createRoot } from 'react-dom/client';
