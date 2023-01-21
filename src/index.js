b8209871b9ea3d22c7bc8f5e7792b22ef9d34a8d
kimballcaroline <kimballcaroline@gmail.com>
2023-01-21 14:58:43 -0500

    Remove BrowserRouter from index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from "react";
-import App from ".//App.jsx";
+import React from 'react';
+import App from './App.jsx';
+// import { BrowserRouter } from 'react-router-dom';
 //import { render } from "react-dom"; --> deprecated, apparently. createRoot method (see below) is part of the new API for rendering React components in the DOM, which was introduced in React 18. This API replaces the old render method, which has been deprecated and will be removed in a future version of React.
-
-
 
 // import "./stylesheets/styles.scss";
 import { createRoot } from 'react-dom/client';
@@ -12,5 +11,7 @@ import { createRoot } from 'react-dom/client';
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 root.render(
-        <App />
-    );
+  // <BrowserRouter>
+  <App />
+  // </BrowserRouter>
+);
