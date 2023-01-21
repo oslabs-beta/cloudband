a7481d16f700600ad2a652915988f3c44b987cef
Greg Jenner <jennergreg733@gmail.com>
2023-01-21 16:09:01 -0500

    incorporate react router to login, signup, and visualizer endpoints

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import App from './App.jsx';
-// import { BrowserRouter } from 'react-router-dom';
-//import { render } from "react-dom"; --> deprecated, apparently. createRoot method (see below) is part of the new API for rendering React components in the DOM, which was introduced in React 18. This API replaces the old render method, which has been deprecated and will be removed in a future version of React.
+import Login from './components/Login.jsx';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { render } from 'react-dom'; //--> deprecated, apparently. createRoot method (see below) is part of the new API for rendering React components in the DOM, which was introduced in React 18. This API replaces the old render method, which has been deprecated and will be removed in a future version of React.
+import { BrowserRouter } from 'react-router-dom';
 
-// import "./stylesheets/styles.scss";
+// // import './stylesheets/styles.scss';
 import { createRoot } from 'react-dom/client';
 
-//render(<App />, document.getElementById("root"));
+// // //render(<App />, document.getElementById("root"));
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 root.render(
-  // <BrowserRouter>
-  <App />
-  // </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
