4e5888e17fa7013cb9f21b7be6d9e96bcd3f99f4
kimballcaroline <kimballcaroline@gmail.com>
2023-01-21 11:08:15 -0500

    Remove Chart import

--- src/components/CPUUtilizationChart.jsx
+++ src/components/CPUUtilizationChart.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Chart from 'chart.js/auto';
+// import Chart from 'chart.js/auto';
 import { Line } from 'react-chartjs-2';
 import '../componentStyling/LineChartStyling.scss';
 
