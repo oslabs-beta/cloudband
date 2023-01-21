66755c215b60f269966cdcb82e7a1c4a52ba935b
kimballcaroline <kimballcaroline@gmail.com>
2023-01-21 15:00:34 -0500

    Add functionality to navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import '../componentStyling/Navbar.scss';
 import React from 'react';
 
-function Navbar() {
+function Navbar(props) {
+  const { setShowPage } = props;
+
+  const handleClick = (event) => {
+    setShowPage(event.target.value);
+  };
+
   return (
     <div className="navbar-wrapper" id="cloud-intro">
       <h1>Cloudband</h1>
@@ -10,10 +16,17 @@ function Navbar() {
           <a href="#">Team</a>
         </li>
         <li>
-          <a href="#">Github</a>
+          <a
+            href="https://github.com/orgs/oslabs-beta/teams/cloudband"
+            target="_blank"
+          >
+            Github
+          </a>
         </li>
         <li>
-          <a href="#">Login</a>
+          <a href="/" onClick={handleClick} value="login">
+            Login
+          </a>
         </li>
       </ul>
     </div>
