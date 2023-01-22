fcba2fa993fb4fe4ffb7c29d52bf6613b8c6f2d7
Greg Jenner <jennergreg733@gmail.com>
2023-01-22 12:47:35 -0500

    add login axios request to receive arn and region

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import { Navigate } from 'react-router-dom';
 
 const Login = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
   const handleSubmit = (event) => {
     event.preventDefault();
     //add get req to get user info and validate ----> TO DO
@@ -12,41 +14,47 @@ const Login = () => {
       })
       .then((response) => {
         console.log(response);
-        // setSignedUp(true);
+        // setLoggedIn(true);
+        // set arn to response.data.newUser.RoleARN
+        // set region to response.data.newUser.region
       })
       .catch((err) => {
         console.log('error in sign up request: ', err);
       });
   };
-
-  return (
-    <div>
-      <h2>Login</h2>
-      <label className="email-label">Enter Email:</label>
-      <input
-        type="text"
-        placeholder="email"
-        id="email"
-        onChange={(e) => {
-          // setUsername(e.target.value);
-          console.log('email: ', e.target.value);
-        }}
-      />
-      <label className="password-label">Enter Password:</label>
-      <input
-        type="password"
-        placeholder="password"
-        id="password"
-        onChange={(e) => {
-          // setPassword(e.target.value);
-          console.log('password: ', e.target.value);
-        }}
-      />
-      <button id="credentials-button" onClick={handleSubmit}>
-        Submit
-      </button>
-    </div>
-  );
+  if (loggedIn) {
+    return <Navigate to="/visualizer" />;
+  }
+  if (!loggedIn) {
+    return (
+      <div>
+        <h2>Login</h2>
+        <label className="email-label">Enter Email:</label>
+        <input
+          type="text"
+          placeholder="email"
+          id="email"
+          onChange={(e) => {
+            // setUsername(e.target.value);
+            console.log('email: ', e.target.value);
+          }}
+        />
+        <label className="password-label">Enter Password:</label>
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          onChange={(e) => {
+            // setPassword(e.target.value);
+            console.log('password: ', e.target.value);
+          }}
+        />
+        <button id="credentials-button" onClick={handleSubmit}>
+          Submit
+        </button>
+      </div>
+    );
+  }
 };
 
 export default Login;
