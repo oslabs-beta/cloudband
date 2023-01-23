b2b94b8eafaaec295b156d20daecae68d9e34f1c
kimballcaroline <kimballcaroline@gmail.com>
2023-01-23 14:23:00 -0500

    Convert region input field to a pre-populated dropdown

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -102,17 +102,54 @@ const Signup = () => {
                 console.log('arn: ', e.target.value);
               }}
             />
-            <label className="region-label">Enter Region:</label>
-            {/* make a drop down https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html */}
-            <input
-              type="text"
-              placeholder="REGION"
+            <label className="region-label">Select Region:</label>
+            <section className="dropdown-wrapper">
+              <select
+                name="region"
               id="region"
               onChange={(e) => {
                 setRegion(e.target.value);
                 console.log('region: ', e.target.value);
               }}
-            />
+                value={region}
+              >
+                <option value="us-east-1">us-east-1</option>
+                <option value="us-east-2">us-east-2</option>
+                <option value="us-west-1">us-west-1</option>
+                <option value="us-west-2">us-west-2</option>
+                <option value="af-south-1">af-south-1</option>
+                <option value="ap-east-1">ap-east-1</option>
+                <option value="ap-south-2">ap-south-2</option>
+                <option value="ap-southeast-3">ap-southeast-3</option>
+                <option value="ap-south-1">ap-south-1</option>
+                <option value="ap-northeast-3">ap-northeast-3</option>
+                <option value="ap-northeast-2">ap-northeast-2</option>
+                <option value="ap-southeast-1">ap-southeast-1</option>
+                <option value="ap-southeast-2">ap-southeast-2</option>
+                <option value="ap-northeast-1">ap-northeast-1</option>
+                <option value="ca-central-1">ca-central-1</option>
+                <option value="eu-central-1">eu-central-1</option>
+                <option value="eu-west-1">eu-west-1</option>
+                <option value="	eu-west-2"> eu-west-2</option>
+                <option value="eu-south-1">eu-south-1</option>
+                <option value="eu-west-3">eu-west-3</option>
+                <option value="	eu-south-2"> eu-south-2</option>
+                <option value="eu-north-1">eu-north-1</option>
+                <option value="eu-central-2">eu-central-2</option>
+                <option value="me-south-1">me-south-1</option>
+                <option value="me-central-1">me-central-1</option>
+                <option value="sa-east-1">sa-east-1</option>
+                <option value="us-gov-east-1">us-gov-east-1</option>
+                <option value="us-gov-west-1">us-gov-west-1</option>
+              </select>
+            </section>
+            <a
+              href="https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.RegionsAndAvailabilityZones.html"
+              target="_blank"
+              className="help-link"
+            >
+              Not sure which region to choose? Check all regions
+            </a>
           </div>
           <div className="column">
             <h3>Step 3:</h3>
