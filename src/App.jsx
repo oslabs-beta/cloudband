dc2c65dce9083e1b7a561690c64df4c0066cfbf5
kimballcaroline <kimballcaroline@gmail.com>
2023-01-25 16:59:00 -0500

    Pass loggedIn states to Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
 
   return (
     <div className="router">
-      <Navbar />
+      <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <hr />
       <div className="routerMain" id="content">
         <Routes>
