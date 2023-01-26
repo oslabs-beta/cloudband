96c74fb61e2b2beae4071b21ca459a2ab8c003c4
Tomas Kim <tk0885@gmail.com>
2023-01-26 12:59:48 -0500

    testing deployment

--- server/models/sessionModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const sessionSchema = new Schema({
-  cookieId: { type: String, required: true, unique: true },
-  createdAt: { type: Date, expires: 90, default: Date.now },
-});
-
-module.exports = mongoose.model('Session', sessionSchema);
--- test/Backend/jest-config-commands.js
+++
@@ -1,6 +0,0 @@
-//npm install --save-dev jest
-
-
-// "jest.pathToJest": "node_modules/jest/bin/jest.js",
-// "jest.enableAutomock": false,
-// "jest.autoEnable": true
