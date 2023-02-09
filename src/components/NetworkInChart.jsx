8bb0ca54dc500d9aae14e4fc8dcd42cc2a5ae707
Cam <102944566+CamSalter@users.noreply.github.com>
2023-02-09 11:15:06 -0500

    updates to networkinchart comments

--- src/components/NetworkInChart.jsx
+++ src/components/NetworkInChart.jsx
@@ -5,24 +5,32 @@ import Options from './LineChartOptions.js';
 import { Colors, ColorsHalfOpacity } from './ColorGenerator.js';
 import '../componentStyling/EC2ChartStyling.scss';
 
+//declare a constant networkInChart and set it equal to an arrow function that takes in props as a parameter
 const NetworkInChart = (props) => {
+  //declare a constant chartData and set it equal to props.chartData
   const { chartData } = props;
 
+  //declare a constant labels and use the map method to iterate over the timestamps array and return a new array of timestamps for each element in the timestamps array
   const labels = chartData.timestamps
     .map((timestamp) => {
+      //convert the timestamp to a date object
       const date = new Date(timestamp);
       // const month = date.getMonth() + 1;
       // const day = date.getDate();
       let hour = date.getHours();
       let minute = date.getMinutes();
+      //if the hour is less than 10, add a 0 to the beginning of the hour
       if (hour < 10) {
         hour = `0${hour}`;
       }
+      //if the minute is less than 10, add a 0 to the beginning of the minute
       if (minute < 10) {
         minute = `0${minute}`;
       }
+      //return the hour and minute
       return `${hour}:${minute}`;
     })
+    //reverse the array in order to display the most recent data first
     .reverse(); //[timestamps]
 
   const datasets = chartData.values
