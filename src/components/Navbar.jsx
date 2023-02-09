3e9cf24bcd3010725dfb5218b9595099b121cd2d
Cam <102944566+CamSalter@users.noreply.github.com>
2023-02-09 11:13:12 -0500

    updates to comments for navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,28 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import '../componentStyling/Navbar.scss';
 
+//declare a constant Navbar and set it equal to an arrow function that takes in props as a parameter
 function Navbar(props) {
+  //declare a constant navigate and set it equal to the invocation of the useNavigate hook
   const navigate = useNavigate();
+  //declare a constant loggedIn and set it equal to the value of the loggedIn property on the props object
   const { loggedIn, setLoggedIn } = props;
 
+  //declare a constant logoutUser and set it equal to an arrow function that takes in event as a parameter
   const logoutUser = (event) => {
+    //make a request to the /logout endpoint and then invoke the setLoggedIn function and pass in false as an argument and then return the invocation of the navigate function and pass in '/login' as an argument
     axios.delete('/logout').then(() => {
       setLoggedIn(false);
       return navigate('/login');
     });
   };
 
+  //declare a constant redirectToVisualizer and set it equal to an arrow function that returns the invocation of the navigate function and pass in '/visualizer' as an argument
   const redirectToVisualizer = () => {
     return navigate('/visualizer');
   };
 
+  //if loggedIn is truthy then return the following JSX
   if (loggedIn) {
     return (
       <div className="navbar-wrapper" id="cloud-intro">
@@ -48,6 +55,7 @@ function Navbar(props) {
         </ul>
       </div>
     );
+    //otherwise, return the following JSX
   } else {
     return (
       <div className="navbar-wrapper" id="cloud-intro">
